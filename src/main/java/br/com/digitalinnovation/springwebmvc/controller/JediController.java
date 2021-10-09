778d9426d2ec05c08ca0a3e8d8862726853072cd
Lais Ziegler <laisziegler@gmail.com>
2021-10-09 07:11:30 -0300

    Initial commit

---
+++ src/main/java/br/com/digitalinnovation/springwebmvc/controller/JediController.java
@@ -0,0 +1,14 @@
+package br.com.digitalinnovation.springwebmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JediController {
+
+    @GetMapping("/jedi")
+    public String jedi(){
+        return "jedi";
+    }
+
+}
